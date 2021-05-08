6d931cbee3cdffc7ca8f4aa519de1eac47f1e245
Andrew Sadovinskiy <sadovinskiy@gmail.com>
2021-05-08 13:19:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import { Command } from 'commander';
 import { createReadStream, createWriteStream } from 'node:fs';
-import Stream from "stream"
+import Stream, { Transform } from "stream"
+import { StringDecoder } from 'string_decoder';
+
+process.stdin.setRawMode(true)
+process.stdin.resume()
+
 const program = new Command();
-program
-  .option('-s, --shift <type>', 'a shift',)
+program.option('-s, --shift <type>', 'a shift',)
   .option('-i, --input <type>', 'an input file',)
   .option('-o, --output <type>', 'an output file',)
   .option('-a, --action <type>', 'an action encode/decode')
@@ -15,9 +19,57 @@ if (!program.opts().action || !program.opts().shift) {
   process.exit(1);
 }
 
+class UppercaseCharacters extends Transform {
+  constructor(options) {
+    super(options)
+    this.message = []
+    // The stream will have Buffer chunks. The
+    // decoder converts these to String instances.
+    this._decoder = new StringDecoder('utf-8')
+  }
+
+  _transform(chunk, encoding, callback) {
+    // Convert the Buffer chunks to String.
+    if (encoding === 'buffer') {
+      chunk = this._decoder.write(chunk)
+    }
+
+    // Exit on CTRL + C.
+    if (chunk === '\u0003') {
+      process.stdout.write(this.message.join(""))
+      process.exit()
+    }
+
+    // Uppercase lowercase letters.
+    if (program.opts().action === "encode") {
+      if (chunk >= 'A' && chunk <= 'Z') {
+        chunk = String.fromCharCode(chunk.charCodeAt(0) + 1);
+        (chunk >= '[') && (chunk = String.fromCharCode(64 + (chunk.charCodeAt(0) - 90)))
+      }
+      if (chunk >= 'a' && chunk <= 'z') {
+        chunk = String.fromCharCode(chunk.charCodeAt(0) + 1);
+        (chunk >= '{') && (chunk = String.fromCharCode(97 + (chunk.charCodeAt(0) - 122)))
+      }
+    } else if(program.opts().action === "decode"){
+      if (chunk >= 'A' && chunk <= 'Z') {
+        chunk = String.fromCharCode(chunk.charCodeAt(0) - 1);
+        (chunk <= '@') && (chunk = String.fromCharCode(90 - (64-chunk.charCodeAt(0))))
+      }
+      if (chunk >= 'a' && chunk <= 'z') {
+        chunk = String.fromCharCode(chunk.charCodeAt(0) - 1);
+        (chunk <= '`') && (chunk = String.fromCharCode(122 - (97-chunk.charCodeAt(0))+1))
+      }
+    }
+    this.message.push(chunk)
+    console.log(this.message.join(""));
+    // Pass the chunk on.
+    callback(null, chunk)
+  }
+}
+
 Stream.pipeline(
   program.opts().input ? createReadStream(program.opts().input, { encoding: "utf-8" }) : process.stdin,
-  //Как добавить обработчик
+  new UppercaseCharacters(),
   program.opts().output ? createWriteStream(program.opts().output) : process.stdout,
   (err) => {
     if (err) {
@@ -26,4 +78,6 @@ Stream.pipeline(
     }
   }
 )
-let r=`Ctrl + C to exit`
+let r = `Ctrl + C to exit`
+
+
